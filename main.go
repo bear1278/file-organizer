e2961388d840818c1b18e714015970c01e0b5d93
bear1278 <grigorijanusik@gmail.com>
2026-04-08 19:05:02 +0300

    add map with rules of organizing

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".txt":  "Documents",
+	".docx": "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
+func main() {
+	for k, v := range DefaultRules {
+		fmt.Println(k, v)
+	}
 }
